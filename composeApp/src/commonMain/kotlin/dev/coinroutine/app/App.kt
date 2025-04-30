b0a98b0d61e46726d3ca72ace545ff00d27b259b
Noor Ul Ain <noorul.ain@ext.careem.com>
2025-04-30 18:29:41 +0500

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/dev/coinroutine/app/App.kt
@@ -0,0 +1,11 @@
+package dev.coinroutine.app
+
+import androidx.compose.runtime.*
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+
+@Composable
+@Preview
+fun App() {
+
+}
---
+++ composeApp/src/commonMain/kotlin/dev/coinroutine/app/Greeting.kt
@@ -0,0 +1,9 @@
+package dev.coinroutine.app
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
